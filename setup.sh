6c1e3498faf4da6a8598d89596419a3db719fed3
Nick Talati <nick.talati@quantworks.com>
2025-02-27 20:20:28 +0000

    some changes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,1 @@
-python -m venv venv
-source venv/bin/activate
-pip install python-language-server pylsp-mypy mypy
-pip install -e ".[core-depless]"
-pip install ".[core]"
+python -m venv venv && source venv/bin/activate && pip install -e ".[dev]"
