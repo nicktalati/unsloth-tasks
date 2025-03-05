a0fc8e407b337379777c55a34914eea4eee275e7
Nick Talati <nicktalati@gmail.com>
2025-03-05 00:45:36 +0000

    improve install script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+set -ex
+VERSION=$(cat .python-version)
+pyenv install -s "$VERSION"
+pyenv local "$VERSION"
 python -m venv venv && source venv/bin/activate && pip install -e ".[dev]"
+set +ex
